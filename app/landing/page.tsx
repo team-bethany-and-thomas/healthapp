cdfc7e18a2e716549f68c8d399ef50c18052fa14
Christopher Ament <chrisament45@gmail.com>
2025-07-21 13:51:55 -0400

    placeholder route and component for /landing

---
+++ app/landing/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LandingPage from '../../components/LandingPage'
+
+const HomePage = () => {
+  return (
+    <main>
+      <LandingPage/>
+    </main>
+  );
+};
+
+export default HomePage;
