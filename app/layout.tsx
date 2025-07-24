cc2a0de95a24942143ff413964cb3229bd00bb7c
Lynjai Jimenez <139661024+IntermediaDesigns@users.noreply.github.com>
2025-07-24 14:32:53 -0400

    feat: add Navbar component to RootLayout and update package dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "../components/features/NavBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,8 +27,12 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning = {true}
       >
+        <>
+        <Navbar />
         {children}
+        </>
       </body>
     </html>
   );
