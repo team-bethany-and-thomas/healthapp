9f984ae5e627e0f176cefe97d316e189785c42df
Ryan Hardin <rhardin378@gmail.com>
2025-07-24 14:52:11 -0500

    pulled from main and merged changes.  Moved my footer into the layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/features/NavBar";
+import { Footer } from "@/components/features/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         <>
           <Navbar />
           {children}
+          <Footer />
         </>
       </body>
     </html>
