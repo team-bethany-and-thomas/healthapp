21d43b4986126e228f2b5d4d2696e44ebb9b495c
Ryan Hardin <rhardin378@gmail.com>
2025-07-24 14:25:28 -0500

    WIP: Footer component styling

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import LandingPage from '../../components/features/LandingPage'
+import React from "react";
+import LandingPage from "../../components/features/LandingPage";
+import { Footer } from "@/components/features/Footer";
 
 const HomePage = () => {
   return (
-    <main>
-      <LandingPage/>
-    </main>
+    <>
+      <main>
+        <LandingPage />
+      </main>
+    </>
   );
 };
 
