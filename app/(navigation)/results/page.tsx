0adfb46535299bd19d0fe7d2161ec046a6d0689b
Richard <richardlwhittington@gmail.com>
2025-07-28 17:06:43 -0500

    ui for provider results page

---
+++ app/(navigation)/results/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Results from "@/components/features/Results";
+
+const ResultsPage = () => {
+  return (
+    <div>
+      <Results />;
+    </div>
+  );
+};
+
+export default ResultsPage;
