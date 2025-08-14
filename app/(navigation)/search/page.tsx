4ae4b85e5de9c507e2d6836f4c0af413bc157c42
Richard Whittington <53500493+kilted2000@users.noreply.github.com>
2025-08-13 19:44:27 -0500

    Richard 126 fe restyle search bar (#130)
    * make border radius consistant
    
    * restyled searchbar

--- app/(navigation)/search/page.tsx
+++ app/(navigation)/search/page.tsx
@@ -1,11 +1,10 @@
 import SearchProviders from '@/components/features/SearchProviders'
-import Searchbar from '@/components/Searchbar'
 import React from 'react'
 
 function page() {
   return (
     <>
-      <Searchbar />
+     
       <SearchProviders />
     </>
   )
